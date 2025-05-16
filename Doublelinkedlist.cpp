aaadfd8127f9783e8ce2ef1628af9191ef0cbaf0
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-05-16 09:41:24 +0700

    Membuat void searcData

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -173,4 +173,19 @@ public:
             i--;
         }
     }
-};
+
+    void searchData()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        int rollNo;
+        cout << "\nEnter the roll number to search: ";
+        cin >> rollNo;
+
+        Node *current = START;
+    }
+};    
