e1be6fdeb6068514065bcd3558c14ef66596b00b
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-05-16 09:34:06 +0700

    Membuat void hapus step 1 yaitu tranverse the list to find the node

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -88,5 +88,15 @@ public:
         cin >> rollNo;
 
         Node *current = START;
+
+        // Step 1: Traverse the list to find the node
+        while (current != NULL && current->noMhs != rollNo)
+            current = current->next;
+
+        if (current == NULL)
+        {
+            cout << "Record not found" << endl;
+            return;
+        }
     }
 };    
