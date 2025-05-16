f390a6775b67d7d62157a50ad2ee8fdb25fc0315
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-05-16 09:14:28 +0700

    Membuat class public and private

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -2,3 +2,23 @@
 #include <iostream>
 #include <string>
 using namespace std; 
+
+class Node
+{
+public:
+    int noMhs;
+    Node *next;
+    Node *prev;
+};
+
+class DoubleLinkedList
+{
+private:
+    Node *START;
+
+public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
+};
